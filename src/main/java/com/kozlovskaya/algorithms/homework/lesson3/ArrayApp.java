dde341eebf252092860091b432d2fadbe42b06e4
Elena Kozlovskaya <lenadk23@gmail.com>
2022-03-07 22:16:52 +0500

    Homework lesson_3 correct

--- src/main/java/com/kozlovskaya/algorithms/homework/lesson3/ArrayApp.java
+++ src/main/java/com/kozlovskaya/algorithms/homework/lesson3/ArrayApp.java
@@ -2,17 +2,22 @@ package com.kozlovskaya.algorithms.homework.lesson3;
 
 public class ArrayApp {
     public static void main(String[] args) {
-        int[] firstArray = {1, 2 ,3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16};
+        int[] firstArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16};
         int[] secondArray = {1, 2, 4, 5, 6};
         int[] thirdArray = {};
+
+        System.out.println("The missing number is: " + findMissingNumber(firstArray));
     }
 
-    public static int findMissingNumber(int[] array){
+    public static int findMissingNumber(int[] array) {
         int result = 0;
-        if(array.length != 0 & array[0] == 1){
-            for (int i = 0; i < array.length-1; i++) {
+        if (array.length == 0) {
+            return result = 1;
+        }
+        if (array[0] == 1) {
+            for (int i = 0; i < array.length - 1; i++) {
                 if (array[i] != array[i + 1] - 1) {
-                    result = array[i];
+                    result = array[i] + 1;
                 }
             }
         }
